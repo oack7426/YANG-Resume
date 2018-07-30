4235e75d4c3ef016f8ed36f5d0d9ea6f28a7fea0
小冰箱 <40828903+oack7426@users.noreply.github.com>
2018-07-30 09:45:44 -0500

    Add files via upload

--- dist/all.js
+++ dist/all.js
@@ -3,3 +3,44 @@ $('.top a').click(function(event) {
     $('html,body').animate({ scrollTop: 0 });
 });
 //回到頂點
+var slideIndex = [1, 1, 1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) { slideIndex[no] = 1 }
+    if (n < 1) { slideIndex[no] = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[no] - 1].style.display = "block";
+}
+
+//
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > $('header').offset().top) {
+            $('.nav').addClass('fixed');
+        } else {
+            $('.nav').removeClass('fixed');
+        }
+        // $(window).scrollTop() >= $('.skill').offset().top {
+        //     $('.skill').fadOut();
+        // }
+    });
+    //menu固定上方
+    $('.showmenu').click(function() {
+        event.preventDefault();
+        $('.nav ul').toggleClass('block');
+    });
+});
